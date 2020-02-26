aa408a260e832379a38d4c9768ca296ce03acb5e
Michel Dávalos <michel.davalos@academicos.udg.mx>
2020-02-26 12:52:15 -0600

    creando repositorio

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  alumnos = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+              public http: HttpClient) {
 
+  }
+
+  getAlumnos() {
+    console.log('getAlumnos');
+
+    this.http.get('/escuela/alumno/')
+    .subscribe(data => {
+      console.log(JSON.stringify(data));
+      this.alumnos = data;
+    }, error => {
+      console.log(JSON.stringify(error));
+    })
   }
 
 }
